58ae667217e217924971ac103756df56ea7443eb
Chris <christopherpack17@gmail.com>
2025-02-11 20:58:24 -0600

    mostly html

--- data/packages.js
+++ data/packages.js
@@ -2,21 +2,21 @@ export const packages = [
   {
     package_id: 1,
     package_title: "",
-    package_description: "",
-  }
+    package_description: ""
+  },
   {
     package_id: 2,
     package_title: "",
-    package_description: "",
-  }
+    package_description: ""
+  },
   {
     package_id: 3,
     package_title: "",
-    package_description: "",
-  }
+    package_description: ""
+  },
   {
     package_id: 4,
     package_title: "",
-    package_description: "",
+    package_description: ""
   }
 ]
