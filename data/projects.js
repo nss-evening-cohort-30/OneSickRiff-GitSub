6f7704e7e1d727f0cb3fb93f73add0423b6035dd
Duncan McPherson <duncan.mcpherson903@gmail.com>
2025-02-11 21:30:07 -0600

    Added initial functionality to main.js

--- data/projects.js
+++ data/projects.js
@@ -1,53 +1,57 @@
 export const projects = [
   {
     project_id: 1,
-    project_title: "",
-    project_description: "",
+    project_title: "Sample 1",
+    project_description: "A very cool description.",
     public: true,
     open: true
   },
   {
     project_id: 2,
-    project_title: "",
-    project_description: "",
-    public: true,
-    open: true
+    project_title: "Sample 2",
+    project_description: "Another much better description.",
+    public: false,
+    open: false
   },
   {
     project_id: 3,
-    project_title: "",
-    project_description: "",
-    public: true,
+    project_title: "Sample 3",
+    project_description: "A third description that is the best.",
+    public: false,
     open: true
   },
   {
     project_id: 4,
-    project_title: "",
-    project_description: "",
+    project_title: "Sample 4",
+    project_description: "A somehow better description than before.",
     public: true,
-    open: true
+    open: false
   },
   {
     project_id: 5,
-    project_title: "",
-    project_description: "",
+    project_title: "Sample 5",
+    project_description: "An arguably worse description.",
     public: true,
     open: true
   }
 ]
 
-export const projectsCard = (object) => {
-  return `
-  <div class="card d-flex flex-row">
-    <h5>${projects.project_title}</h5>
-    <p>${projects.project_description}</p>
-  </div>`
-}
+// const projectsCard = (object) => {
+//   return `
+//   <div class="card d-flex flex-row">
+//     <h5>${projects.project_title}</h5>
+//     <p>${projects.project_description}</p>
+//   </div>`
+// }
 
-export const renderedProjects = (array) => {
-  let finalRender = ''
-  array.forEach(project => {
-    finalRender += projectsCard(project)
-  });
-  return finalRender
-}
+// const renderedProjects = (array) => {
+//   let finalRender = ''
+//   array.forEach(project => {
+//     finalRender += projectsCard(project)
+//   });
+//   return finalRender
+// }
+
+// const sort = () => {
+
+// }
--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,58 @@ import { projects } from "../data/projects";
 import { repos } from "../data/repos";
 import { profile } from "../components/profile";
 
+const projectsCard = (object) => {
+  return `
+  <div class="card d-flex flex-row">
+    <h5>${projects.project_title}</h5>
+    <p>${projects.project_description}</p>
+  </div>`
+}
 
+const createProject = () => {
+
+}
+
+const renderedCards = (array) => {
+  let finalRender = ''
+  array.forEach(item => {
+    finalRender += projectsCard(item)
+  });
+  return finalRender
+}
+
+const renderedPinnedRepos = (array) => {
+  let finalRender = ''
+  const pinnedRepo = array.filter((item) => item.pinned == true)
+  pinnedRepo.forEach(repo => {
+    finalRender += repoCard(repo)
+  });
+  return finalRender
+}
+
+const createData = (event) => {
+  e.preventDefault();
+  if (event.target.id === "project-submit-btn") {
+
+    renderToDom("#content-list", renderedCards(projects))
+  }
+  if (event.target.id === "repo-submit-btn") {
+    renderToDom("#content-list", renderedCards(repos))
+  }
+  if (event.target.id === "pinned-repo-submit-btn") {
+    renderToDom("#content-list", renderedPinnedRepos(repos))
+  }
+  if (event.target.id === "package-submit-btn") {
+    renderToDom("#content-list", renderedCards(packages))
+  }
+   
+}
 
 // START APP
 const startApp = () => {
 
+  document.querySelector('#create-form').addEventListener('click', createData);
+  
 }
 
 startApp();
