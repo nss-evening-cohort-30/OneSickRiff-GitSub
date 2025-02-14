d9b7bbeee422e97738e710fa88a3c899eeb8e264
Chris <christopherpack17@gmail.com>
2025-02-13 19:53:19 -0600

    delete

--- scripts/main.js
+++ scripts/main.js
@@ -1,89 +1,28 @@
-import { renderToDom } from "../utils/renderToDom";
-import { packages } from "../data/packages";
-import { projects } from "../data/projects";
-import { repos } from "../data/repos";
-import { repoList } from "../data/repos";
-import { profile } from "../components/profile";
+import { renderToDom } from "/utils/renderToDom.js";
+import { packages } from "/data/packages.js";
+import { projects } from "/data/projects.js";
+import { repos } from "/data/repos.js";
+import { repoList } from "/data/repos.js";
 
-let projectTotal = projects.length;
-let repoTotal = repos.length;
-let packageTotal = packages.length;
-
-const projectsCard = (object) => {
+const projectsCard = (projects) => {
   return `
-  <div class="card d-flex flex-row">
+  <div class="card m-1" style="width: 18rem; d-flex flex-row">
     <h5>${projects.project_title}</h5>
     <p>${projects.project_description}</p>
   </div>`
 }
 
-const reposCard = (object) => {
-  return `
-  <div class="card d-flex flex-row">
-    <h5>${repos.repo_title}</h5>
-    <p>${repos.repo_description}</p>
-  </div>`
+const createProject = () => {
+
 }
 
-const packagesCard = (object) => {
-  return `
-  <div class="card d-flex flex-row">
-    <h5>${packages.package_title}</h5>
-    <p>${packages.package_description}</p>
-  </div>`
-}
-
-const createItem = (type) => {
-  if (type == proj) {
-    const projectObj = {
-      project_id: projectTotal += 1,
-      project_title: document.querySelector('#name').value,
-      project_description: document.querySelector('#desc').value,
-      public: false,
-      open: true
-    }
-    projects.push(projectObj)
-  } else if (type == rep) {
-    const reposObj = {
-      repo_id: repoTotal += 1,
-      repo_name: document.querySelector('#name').value,
-      repo_description: document.querySelector('#desc').value,
-      pinned: false,
-      star: true
-    }
-    repos.push(reposObj)
-  } else if (type == pinnedRep) {
-    const reposObj = {
-      repo_id: repoTotal += 1,
-      repo_name: document.querySelector('#name').value,
-      repo_description: document.querySelector('#desc').value,
-      pinned: true,
-      star: true
-    }
-    repos.push(reposObj)  
-  } else if (type == pack) {
-    const packageObj = {
-      package_id: packageTotal += 1,
-      package_title: document.querySelector('#name').value,
-      package_description: document.querySelector('#desc').value
-    }
-    packages.push(packageObj)
-  }
-}
-
-const renderedCards = (array, type) => {
-  let finalRender = ''
+const renderedCards = (array) => {
+  let finalRender = '';
   array.forEach(item => {
-    if (type == proj) {
-      finalRender += projectsCard(item)
-    } else if (type == rep) {
-      finalRender += reposCard(item)
-    } else if (type == pack) {
-      finalRender += packagesCard(item)
-    }
+    finalRender += projectsCard(item)
   });
-  return finalRender
-}
+  renderToDom('#cards', finalRender);
+};
 
 const renderedPinnedRepos = (array) => {
   let finalRender = ''
@@ -97,38 +36,28 @@ const renderedPinnedRepos = (array) => {
 const createData = (event) => {
   e.preventDefault();
   if (event.target.id === "project-submit-btn") {
-    createItem(proj)
-    document.querySelector('#name').value = ''
-    document.querySelector('#desc').value = ''
-    renderToDom("#content-list", renderedCards(projects, proj))
+
+    renderToDom("#content-list", renderedCards(projects))
   }
   if (event.target.id === "repo-submit-btn") {
-    createItem(rep)
-    document.querySelector('#name').value = ''
-    document.querySelector('#desc').value = ''
-    renderToDom("#content-list", renderedCards(repos, rep))
+    renderToDom("#content-list", renderedCards(repos))
   }
   if (event.target.id === "pinned-repo-submit-btn") {
-    createItem(pinnedRep)
-    document.querySelector('#name').value = ''
-    document.querySelector('#desc').value = ''
     renderToDom("#content-list", renderedPinnedRepos(repos))
   }
   if (event.target.id === "package-submit-btn") {
-    createItem(pack)
-    document.querySelector('#name').value = ''
-    document.querySelector('#desc').value = ''
-    renderToDom("#content-list", renderedCards(packages, pack))
+    renderToDom("#content-list", renderedCards(packages))
   }
    
 }
 
-
 // START APP
 const startApp = () => {
+  renderedCards(projects);
+  ElementInternals 
 
+  document.querySelector("#cards")
   document.querySelector('#create-form').addEventListener('click', createData);
-  
 }
 
 startApp();
